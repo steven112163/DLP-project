fd9f44c8721deb34f40ec511d92de4d7a604a30c
steven112163 <steven112163@gmail.com>
2021-05-28 01:00:06 +0800

    Move models to model.py and add transformer encoder

---
+++ model.py
@@ -0,0 +1,122 @@
+from typing import Tuple
+import torch.nn as nn
+import torch
+
+
+class Time2Vector(nn.Module):
+    def __init__(self, seq_len: int):
+        super(Time2Vector, self).__init__()
+        self.weights_non_periodic = nn.Parameter(data=torch.randn(seq_len))
+        self.bias_non_periodic = nn.Parameter(data=torch.randn(seq_len))
+        self.weights_periodic = nn.Parameter(data=torch.randn(seq_len))
+        self.bias_periodic = nn.Parameter(data=torch.randn(seq_len))
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """
+        Forward propagation
+        :param inputs: stock price [batch_size, seq_len, 5]
+        :return: time feature [batch_size, seq_len, 2]
+        """
+        x = torch.mean(inputs[:, :, 0:4], dim=-1)
+        time_non_periodic = x * self.weights_non_periodic + self.bias_non_periodic
+        time_non_periodic = torch.unsqueeze(time_non_periodic, dim=-1)
+
+        time_periodic = torch.sin(x * self.weights_periodic + self.bias_periodic)
+        time_periodic = torch.unsqueeze(time_periodic, dim=-1)
+
+        return torch.cat([time_non_periodic, time_periodic], dim=-1)
+
+
+class SingleHeadAttention(nn.Module):
+    def __init__(self, attn_dim: int):
+        super(SingleHeadAttention, self).__init__()
+        # TODO
+        self.attn_dim = attn_dim
+        self.query = nn.Linear(in_features=7, out_features=self.attn_dim)
+        self.key = nn.Linear(in_features=7, out_features=self.attn_dim)
+        self.value = nn.Linear(in_features=7, out_features=self.attn_dim)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, inputs: Tuple[torch.Tensor, ...]) -> torch.Tensor:
+        """
+        Forward propagation
+        :param inputs: (query, key, value), each dimension is [batch_size, seq_len, 7]
+        :return: attention weight [batch_size, seq_len, attn_dim]
+        """
+        q = self.query(inputs[0])
+        k = self.key(inputs[1])
+        v = self.value(inputs[2])
+
+        attn_weights = torch.matmul(q, torch.transpose(k, 1, 2))
+        attn_weights = torch.div(attn_weights, self.attn_dim ** 0.5)
+        attn_weights = self.softmax(attn_weights)
+        return torch.matmul(attn_weights, v)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, attn_dim: int, num_heads: int):
+        super(MultiHeadAttention, self).__init__()
+        self.attn_heads = list()
+        for _ in range(num_heads):
+            self.attn_heads.append(SingleHeadAttention(attn_dim=attn_dim))
+        self.linear = nn.Linear(in_features=num_heads * attn_dim, out_features=7)
+
+    def forward(self, inputs: Tuple[torch.Tensor, ...]) -> torch.Tensor:
+        """
+        Forward propagation
+        :param inputs: (query, key, value), each dimension is [batch_size, seq_len, 7]
+        :return: attention weight [batch_size, seq_len, 7]
+        """
+        attn = [head.forward(inputs=inputs) for head in self.attn_heads]
+        concat_attn = torch.cat(attn, dim=-1)
+        return self.linear(concat_attn)
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self,
+                 batch_size: int,
+                 seq_len: int,
+                 attn_dim: int,
+                 num_heads: int,
+                 dropout_rate: float,
+                 hidden_size: int):
+        super(TransformerEncoder, self).__init__()
+        self.first = nn.Sequential(
+            MultiHeadAttention(attn_dim=attn_dim,
+                               num_heads=num_heads),
+            nn.Dropout(p=dropout_rate)
+        )
+        self.second = nn.Sequential(
+            nn.LayerNorm(normalized_shape=[batch_size, seq_len, 7]),
+            nn.Linear(in_features=7,
+                      out_features=hidden_size),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=hidden_size,
+                      out_features=7),
+            nn.Dropout(p=dropout_rate)
+        )
+        self.layer_normalization = nn.LayerNorm(normalized_shape=[batch_size, seq_len, 7])
+
+    def forward(self, inputs: Tuple[torch.Tensor, ...]) -> torch.Tensor:
+        """
+        Forward propagation
+        :param inputs: (query, key, value), each dimension is [batch_size, seq_len, 7]
+        :return: embedding [batch_size, seq_len, 7]
+        """
+        query = inputs[0]
+        partial_results = self.first(inputs)
+        partial_results = self.second(query + partial_results)
+        return self.layer_normalization(query + partial_results)
+
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        # TODO
+
+    def forward(self):
+        """
+        Forward propagation
+        :return:
+        """
+        # TODO
--- project.py
+++ project.py
@@ -1,111 +1,9 @@
 from argument_parser import parse_arguments
+from model import Network
+from data_loader import DataLoader
 import torch.nn as nn
-import numpy as np
 import torch
 import sys
-
-
-class Time2Vector:
-    def __init__(self, seq_len, **kwargs):
-        super(Time2Vector, self).__init__()
-        # TODO
-        self.seq_len = seq_len
-        self.weights_nonperiodic = nn.Parameter(torch.zeros(seq_len))
-        self.bias_nonperiodic = nn.Parameter(torch.zeros(seq_len))
-        self.weights_periodic = nn.Parameter(torch.zeros(seq_len))
-        self.bias_periodic = nn.Parameter(torch.zeros(seq_len))
-
-    def forward(self, x):
-        """
-        Forward propagation
-        :return:
-        """
-        # TODO
-        x = torch.mean(x[:, :, 0:4], dim=-1)
-        time_nonperiodic = x * self.weights_nonperiodic + self.bias_nonperiodic
-        time_nonperiodic = np.expand_dims(time_nonperiodic, axis=-1)
-
-        time_periodic = torch.sin(x * self.weights_periodic + self.bias_periodic)
-        time_periodic = np.expand_dims(time_periodic, axis=-1)
-
-        return np.concatenate([time_nonperiodic, time_periodic], axis=-1)
-
-
-class SingleHeadAttention(nn.Module):
-    def __init__(self, d_k, d_v, attribute=7):
-        super(SingleHeadAttention, self).__init__()
-        # TODO
-        self.d_k = d_k
-        self.d_v = d_v
-        self.query = nn.Linear(attribute, self.d_k)
-        self.key = nn.Linear(attribute, self.d_k)
-        self.value = nn.Linear(attribute, self.d_v)
-
-    def forward(self, x):  # x=[q,k,v]
-        """
-        Forward propagation
-        :return:
-        """
-        # TODO
-        q = self.query(x[0])
-        k = self.key(x[1])
-        v = self.value(x[2])
-
-        attn_weights = np.matmul(q, k.T)
-        for w in attn_weights:
-            w /= np.sqrt(self.d_k)
-        attn_weights = nn.Softmax(attn_weights, dim=-1)
-        out = np.matmul(attn_weights, v)
-        return out
-
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, d_k, d_v, n_heads, attribute=288):
-        super(MultiHeadAttention, self).__init__()
-        # TODO
-        self.d_k = d_k
-        self.d_v = d_v
-        self.n_heads = n_heads
-        self.attn_heads = list()
-        for n in range(self.n_heads):
-            self.attn_heads.append(SingleHeadAttention(self.d_k, self.d_v))
-        self.linear = nn.Linear(attribute, 7)
-
-    def forward(self, x):
-        """
-        Forward propagation
-        :return:
-        """
-        # TODO
-        attn = [self.attn_heads[i](x) for i in range(self.attn_heads)]
-        concat_attn = np.concatenate(attn, axis=-1)
-        out = self.linear(concat_attn)
-
-
-class TransformerEncoder(nn.Module):
-    def __init__(self):
-        super(TransformerEncoder, self).__init__()
-        # TODO
-
-    def forward(self):
-        """
-        Forward propagation
-        :return:
-        """
-        # TODO
-
-
-class Network(nn.Module):
-    def __init__(self):
-        super(Network, self).__init__()
-        # TODO
-
-    def forward(self):
-        """
-        Forward propagation
-        :return:
-        """
-        # TODO
 
 
 def train() -> None:
@@ -155,6 +53,9 @@ def main() -> None:
     """
     # Parse arguments
     args = parse_arguments()
+    info_log(f'Number of epochs: {args.epochs}')
+    info_log(f'Batch size: {args.batch_size}')
+    info_log(f'Sequence length (consecutive days): {args.seq_len}')
     global verbosity
     verbosity = args.verbosity
 
